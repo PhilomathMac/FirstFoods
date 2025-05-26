1c56f5ff09f9d0374faa2716ea6669c93c21d0bf
McKenzie Macdonald <kenziemacd@icloud.com>
2025-05-25 17:38:00 -0700

    Initial Commit

---
+++ FirstFoods/FirstFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstFoodsApp.swift
+//  FirstFoods
+//
+//  Created by McKenzie Macdonald on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
